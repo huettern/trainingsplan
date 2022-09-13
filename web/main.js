93c429852bd93514863f61dae051b8bedea7e417
Adrian Egloff <adrian.egloff@bsi-software.com>
2022-09-13 21:57:22 +0200

    Format javascrip code with vs code formatter

--- web/main.js
+++ web/main.js
@@ -8,7 +8,7 @@
 class Trainingsplan {
 
 	constructor() {
-		this.initial = [{'group':0, 'event': 5}, {'group':1, 'event': 4}, {'group':2, 'event': 1}, {'group':3, 'event': 3}];
+		this.initial = [{ 'group': 0, 'event': 5 }, { 'group': 1, 'event': 4 }, { 'group': 2, 'event': 1 }, { 'group': 3, 'event': 3 }];
 
 		this.startDate = '19.10.2020';
 		this.trainingDays = [0, 4];
@@ -18,9 +18,9 @@ class Trainingsplan {
 		this.events = ['Re', 'Tr', 'Bo', 'Sr', 'Sp', 'Ba'];
 		this.warmup = ['Andrin', 'Lian', 'Stephan', 'Cyrille', 'Melvin', 'Maurice', 'Noah B.', 'Adi', 'Jens', 'Lars', 'Nicola', 'Tim']
 		this.weeklyIncrement = 2;
-		this.halls = [ ['RE', 'TR', 'BO', 'SR'], ['BA', 'SP'] ];
-		this.specials = [ { 'id': 'airtrack', 'period': 4 } ];
-		this.competitionTraining = [('K1', 'RE'), ('K2', 'SP'), ('K3',' SR'), ('K5+', 'BO')];
+		this.halls = [['RE', 'TR', 'BO', 'SR'], ['BA', 'SP']];
+		this.specials = [{ 'id': 'airtrack', 'period': 4 }];
+		this.competitionTraining = [('K1', 'RE'), ('K2', 'SP'), ('K3', ' SR'), ('K5+', 'BO')];
 
 		this.trainingsPerWeek = this.trainingDays.length;
 		// offset is in unit [training number], not days!
@@ -32,7 +32,7 @@ class Trainingsplan {
 	getTrainingMap(initial, rotation) {
 		var ret = []
 		for (const m in initial) {
-			ret.push( {'group': initial[m].group, 'event': (initial[m].event+rotation)%this.events.length} );
+			ret.push({ 'group': initial[m].group, 'event': (initial[m].event + rotation) % this.events.length });
 		}
 		return ret;
 	}
@@ -49,20 +49,20 @@ class Trainingsplan {
 
 	getDateFromOffset(offset) {
 		var off = moment(this.startDate, "DD.MM.YYYY");
-		var dt_days = 7*Math.floor(offset/this.trainingsPerWeek) + this.trainingDays[(offset%this.trainingsPerWeek)];
+		var dt_days = 7 * Math.floor(offset / this.trainingsPerWeek) + this.trainingDays[(offset % this.trainingsPerWeek)];
 		return off.add('days', dt_days).toDate();
 	}
 
 	getTrainingInitial(initial, offset, weekly_incr) {
 		let n_weeks = Math.floor(offset / this.trainingsPerWeek);
 		let remainder = offset % this.trainingsPerWeek;
-		let inc = n_weeks*(weekly_incr + this.trainingsPerWeek*this.eventsPerDay) + remainder*this.eventsPerDay;
+		let inc = n_weeks * (weekly_incr + this.trainingsPerWeek * this.eventsPerDay) + remainder * this.eventsPerDay;
 
-	 	// console.log(`inc for offset ${offset} = ${inc} (${n_weeks} weeks, ${remainder} days)`)
+		// console.log(`inc for offset ${offset} = ${inc} (${n_weeks} weeks, ${remainder} days)`)
 
 		var ret = []
 		for (const m in initial) {
-			ret.push( {'group': initial[m].group, 'event': (initial[m].event+inc)%this.events.length} );
+			ret.push({ 'group': initial[m].group, 'event': (initial[m].event + inc) % this.events.length });
 		}
 		return ret;
 	}
@@ -70,48 +70,48 @@ class Trainingsplan {
 	startDateToOffset(date) {
 		var now = moment(date, "DD.MM.YYYY");
 		var off = moment(this.startDate, "DD.MM.YYYY");
-	  	var delta = now-off;
-	  	return Math.floor(delta/(1000*60*60*24)/7) * this.trainingsPerWeek;
+		var delta = now - off;
+		return Math.floor(delta / (1000 * 60 * 60 * 24) / 7) * this.trainingsPerWeek;
 	}
 
 	genWeek(mapping, offset) {
 		let initial = tp.getTrainingInitial(mapping, offset, tp.weeklyIncrement);
 
-		let warm0 = this.warmup[offset%this.warmup.length];
-		let warm1 = this.warmup[(offset+1)%this.warmup.length];
-		let options = {weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
-        let monday = tp.getDateFromOffset(offset).toLocaleString('de-CH', options);
-        let friday = tp.getDateFromOffset(offset+1).toLocaleString('de-CH', options);
-        let group0 = this.groups[0];
-        let group1 = this.groups[1];
-        let group2 = this.groups[2];
-        let group3 = this.groups[3];
-        let g0e0 = this.getEventByGroup(initial, 0, 0);
-        let g0e1 = this.getEventByGroup(initial, 1, 0);
-        let g0e2 = this.getEventByGroup(initial, 2, 0);
-        let g0e3 = this.getEventByGroup(initial, 3, 0);
-        let g0e4 = this.getEventByGroup(initial, 4, 0);
-        let g0e5 = this.getEventByGroup(initial, 5, 0);
-        let g1e0 = this.getEventByGroup(initial, 0, 1);
-        let g1e1 = this.getEventByGroup(initial, 1, 1);
-        let g1e2 = this.getEventByGroup(initial, 2, 1);
-        let g1e3 = this.getEventByGroup(initial, 3, 1);
-        let g1e4 = this.getEventByGroup(initial, 4, 1);
-        let g1e5 = this.getEventByGroup(initial, 5, 1);
-        let g2e0 = this.getEventByGroup(initial, 0, 2);
-        let g2e1 = this.getEventByGroup(initial, 1, 2);
-        let g2e2 = this.getEventByGroup(initial, 2, 2);
-        let g2e3 = this.getEventByGroup(initial, 3, 2);
-        let g2e4 = this.getEventByGroup(initial, 4, 2);
-        let g2e5 = this.getEventByGroup(initial, 5, 2);
-        let g3e0 = this.getEventByGroup(initial, 0, 3);
-        let g3e1 = this.getEventByGroup(initial, 1, 3);
-        let g3e2 = this.getEventByGroup(initial, 2, 3);
-        let g3e3 = this.getEventByGroup(initial, 3, 3);
-        let g3e4 = this.getEventByGroup(initial, 4, 3);
-        let g3e5 = this.getEventByGroup(initial, 5, 3);
+		let warm0 = this.warmup[offset % this.warmup.length];
+		let warm1 = this.warmup[(offset + 1) % this.warmup.length];
+		let options = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
+		let monday = tp.getDateFromOffset(offset).toLocaleString('de-CH', options);
+		let friday = tp.getDateFromOffset(offset + 1).toLocaleString('de-CH', options);
+		let group0 = this.groups[0];
+		let group1 = this.groups[1];
+		let group2 = this.groups[2];
+		let group3 = this.groups[3];
+		let g0e0 = this.getEventByGroup(initial, 0, 0);
+		let g0e1 = this.getEventByGroup(initial, 1, 0);
+		let g0e2 = this.getEventByGroup(initial, 2, 0);
+		let g0e3 = this.getEventByGroup(initial, 3, 0);
+		let g0e4 = this.getEventByGroup(initial, 4, 0);
+		let g0e5 = this.getEventByGroup(initial, 5, 0);
+		let g1e0 = this.getEventByGroup(initial, 0, 1);
+		let g1e1 = this.getEventByGroup(initial, 1, 1);
+		let g1e2 = this.getEventByGroup(initial, 2, 1);
+		let g1e3 = this.getEventByGroup(initial, 3, 1);
+		let g1e4 = this.getEventByGroup(initial, 4, 1);
+		let g1e5 = this.getEventByGroup(initial, 5, 1);
+		let g2e0 = this.getEventByGroup(initial, 0, 2);
+		let g2e1 = this.getEventByGroup(initial, 1, 2);
+		let g2e2 = this.getEventByGroup(initial, 2, 2);
+		let g2e3 = this.getEventByGroup(initial, 3, 2);
+		let g2e4 = this.getEventByGroup(initial, 4, 2);
+		let g2e5 = this.getEventByGroup(initial, 5, 2);
+		let g3e0 = this.getEventByGroup(initial, 0, 3);
+		let g3e1 = this.getEventByGroup(initial, 1, 3);
+		let g3e2 = this.getEventByGroup(initial, 2, 3);
+		let g3e3 = this.getEventByGroup(initial, 3, 3);
+		let g3e4 = this.getEventByGroup(initial, 4, 3);
+		let g3e5 = this.getEventByGroup(initial, 5, 3);
 
-        const template = `
+		const template = `
         <table class="table table-hover tpl" id="table-trainingsplan">
 		<thead>
 		<tr>
@@ -166,29 +166,29 @@ class Trainingsplan {
 		</table>
         `;
 
-        return template;
+		return template;
 	}
 }
 
 const tp = new Trainingsplan();
 
 function isMonday() {
-	let dow = (moment().weekday()+6)%7;
+	let dow = (moment().weekday() + 6) % 7;
 	return dow == 0;
 }
 
 function isFriday() {
-	let dow = (moment().weekday()+6)%7;
+	let dow = (moment().weekday() + 6) % 7;
 	return dow == 4;
 }
 
 function render() {
 	document.getElementById("trainingsplan").innerHTML = tp.genWeek(tp.initial, tp.currentOffset);
-	
+
 	// emphasize day
-	let off_today = tp.startDateToOffset(moment().format("DD.MM.YYYY"));	
-	if(off_today == tp.currentOffset) {
-		let dow = (moment().weekday()+6)%7;
+	let off_today = tp.startDateToOffset(moment().format("DD.MM.YYYY"));
+	if (off_today == tp.currentOffset) {
+		let dow = (moment().weekday() + 6) % 7;
 		let i = 0;
 		for (const m in tp.trainingDays) {
 			if (tp.trainingDays[m] == dow) {
@@ -201,7 +201,7 @@ function render() {
 
 	// specials
 	for (const m in tp.specials) {
-		if( (tp.currentOffset/tp.trainingsPerWeek) % tp.specials[m].period == 0) {
+		if ((tp.currentOffset / tp.trainingsPerWeek) % tp.specials[m].period == 0) {
 			document.getElementById(tp.specials[m].id).style.display = '';
 		}
 		else {
@@ -213,20 +213,20 @@ function render() {
 	document.getElementById('spannreck').style.display = 'none';
 	var table = document.getElementById("table-trainingsplan");
 	for (var i = 0, row; row = table.rows[i]; i++) {
-		if(table.rows[i].innerHTML.includes('K5+')) {
+		if (table.rows[i].innerHTML.includes('K5+')) {
 			for (var j = 0, col; col = row.cells[j]; j++) {
-				if(row.cells[j].innerHTML.includes('Re')) {
+				if (row.cells[j].innerHTML.includes('Re')) {
 					row.cells[j].classList.add("alert-warning");
-					if(isMonday() && j < 4) document.getElementById('spannreck').style.display = '';
-					if(isFriday() && j >= 4) document.getElementById('spannreck').style.display = '';
+					if (isMonday() && j < 4) document.getElementById('spannreck').style.display = '';
+					if (isFriday() && j >= 4) document.getElementById('spannreck').style.display = '';
 				}
-			}  
+			}
 		}
 	}
 
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 	console.log("Hello World!");
 	// console.log(tp.startDate);
 	// let options = {weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' };
@@ -250,14 +250,14 @@ $(document).ready(function(){
 
 	render();
 	document.getElementById("btn-left").disabled = true;
- }); 
+});
 
 
 function btnPrevWeek() {
-	if(document.getElementById("btn-left").disabled) return;
+	if (document.getElementById("btn-left").disabled) return;
 	tp.currentOffset = tp.currentOffset - tp.trainingsPerWeek;
 	render();
-	if(tp.currentOffset < 2) {
+	if (tp.currentOffset < 2) {
 		document.getElementById("btn-left").disabled = true;
 	}
 }
